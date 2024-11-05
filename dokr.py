c47e8b651212740f1a42de22c2c6ea82e7901de7
JakubTheDeveloper <jakub.krysakowski@gmail.com>
2024-11-05 21:55:49 +0100

    Update dokr.py

--- dokr.py
+++ dokr.py
@@ -6,6 +6,7 @@
 import docker
 import subprocess
 import argparse
+from colorama import Fore, Back, Style
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-a", "--all", action='store_const', const=True)
@@ -14,32 +15,41 @@ args = parser.parse_args()
 client = docker.from_env()
 
 containers = client.containers.list()
+containers.sort(key=lambda x: x.name, reverse=False)
 
 i = 0
 choiceList = []
+
 for c in containers:  
-  if args.all or 'php' in c.name or 'systemd' in c.name or 'db' in c.name:
+  if args.all or 'php' in c.name or 'systemd' in c.name or 'db' in c.name or 'nginx' in c.name:
     print(i, c.name)
     choiceList.append(c.name)
     i = i + 1
 
 print("\n")
-print("Enter container number: ")
 
-choice = input()
+print(Style.DIM + "If the container you are looking for is not on the list, run the command with -a.")
+
+print(Style.NORMAL + Fore.GREEN + "Enter container number: ")
+
+try:
+  choice = input()
+except KeyboardInterrupt:
+  print(Fore.RESET + "\nGoodbye")
+  exit()
+
+print(Fore.RESET)
 
 try:
   choice = int(choice)
 except ValueError as ve:
-  print("Not a number.")
+  print(Fore.RED + "Not a number." + Fore.RESET)
   exit()
 
 if choice < 0 or choice >= i:
-  print("Wrong choice")
+  print(Fore.RED + "Wrong choice" + Fore.RESET)
   exit()
 
 cmdStr = "docker exec -it " + choiceList[choice] + " bash"
 subprocess.run(cmdStr, shell=True)
 exit()
-
-
